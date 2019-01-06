b862b40c90fd36c8463339836f7eff07f848bd9b
Rajarshi Basak <rajarshi.basak@colorado.edu>
2019-01-06 02:06:01 -0700

    Create plus_minus.py

---
+++ Algorithms/plus_minus.py
@@ -0,0 +1,28 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the plusMinus function below.
+def plusMinus(arr):
+    pos = 0
+    neg = 0
+    zer = 0
+    for i in range(len(arr)):
+        if (arr[i] > 0):
+            pos += 1
+        elif (arr [i] < 0):
+            neg += 1
+        elif (arr[i] == 0):
+            zer += 1
+    print(pos/len(arr))
+    print(neg/len(arr))
+    print(zer/len(arr))
+
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
